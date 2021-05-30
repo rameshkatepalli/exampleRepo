4eb5f974e7a793d27fae1c4d2498756a0ef3fff1
rameshkatepalli <rameshram661808@gmail.com>
2021-05-30 12:43:02 +0530

    new project created

---
+++ src/main/java/com/mindtree/oauth2/main/WebAppWithOauth2Application.java
@@ -0,0 +1,13 @@
+package com.mindtree.oauth2.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppWithOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppWithOauth2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/mindtree/oauth2/main/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.mindtree.oauth2.main.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("user")
+    @ResponseBody
+    public Principal user(Principal principal) {
+        return principal;
+    }
+	
+	@RequestMapping("/index")
+	public String homePage() {
+		System.out.println("hi");
+		return "index";
+	}
+}
