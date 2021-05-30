654bf97575df18f0e0a431e651e554445ffe9234
sanjay <sanjaycsa09@gmail.com>
2021-05-30 13:24:59 +0530

    dd

--- src/main/java/com/mindtree/oauth2/main/WebAppWithOauth2Application.java
+++ src/main/java/com/mindtree/oauth2/main/WebAppWithOauth2Application.java
@@ -2,7 +2,7 @@ package com.mindtree.oauth2.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//sann
 @SpringBootApplication
 public class WebAppWithOauth2Application {
 
